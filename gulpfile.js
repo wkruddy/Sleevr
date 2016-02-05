e5fc6c4123c92155dbbb85ea7855d0d6abc1a9e6
Kyle Ruddy <wkruddy@gmail.com>
2016-02-04 22:49:13 -0500

    Added BrowserSync support, and a small express webapp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 'use strict';
 
 var gulp = require('gulp'),
-		reload = require('gulp-livereload'),
-		concat = require('gulp-concat'),
+    reload = require('gulp-livereload'),
+    concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-		rename = require('gulp-rename'),
+    rename = require('gulp-rename'),
     jshint = require('gulp-jshint'),
-		clean = require('gulp-clean'),
-		utils = require('gulp-util'),
+    clean = require('gulp-clean'),
+    utils = require('gulp-util'),
     nano = require('gulp-cssnano'),
-		sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload,
+    autoprefixer = require('gulp-autoprefixer'),
+    nodemon = require('gulp-nodemon');
 
 var paths = {
   sourceJs: 'source/js/*.js',
@@ -18,11 +22,41 @@ var paths = {
   buildCss: 'build/css'
 };
 
+// Initialize BrowserSync with the port BS should run on,
+// and the express server port
+gulp.task('browser:sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    proxy: 'http://localhost:9000',
+    port: 5000
+  });
+});
+
+// Task for starting nodemon to use a local express server
+gulp.task('nodemon', function (callback) {
+
+  var started = false;
+
+  return nodemon({
+    script: 'app.js',
+    ignore: [
+      'gulpfile.js',
+      'node_modules/'
+    ]
+  }).on('start', function () {
+    // Standard pattern for making sure nodemon doesn't init a bunch
+
+    if (!started) {
+      callback();
+      started = true;
+    }
+  });
+});
+
 gulp.task('lint', function(){
   return gulp.src(paths.sourceJs)
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
-        .pipe(reload());
+        .pipe(reload({stream: true}));
 });
 
 gulp.task('scripts:concat-min', ['lint'], function(){
@@ -35,14 +69,14 @@ gulp.task('scripts:concat-min', ['lint'], function(){
         }))
         .pipe(uglify())
         .pipe(gulp.dest('build/js'))
-        .pipe(reload());
+        .pipe(reload({stream: true}));
 });
 
 
 gulp.task('sass:compile', function(){
-  return gulp.src(['source/sass/styles.scss',
-                  'node_modules/bootstrap/dist/css/bootstrap.css',
-                  'node_modules/bootstrap/dist/css/bootstrap-theme.css'
+  return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css',
+                  'node_modules/bootstrap/dist/css/bootstrap-theme.css',
+                  'source/sass/styles.scss'
         ])
         .pipe(sass())
         .pipe(gulp.dest(paths.buildCss));
@@ -58,20 +92,17 @@ gulp.task('sassy:min', ['sass:compile'], function(){
         .pipe(gulp.dest(paths.buildCss));
 });
 
+
 gulp.task('clean', function(){
   return gulp.src(['build/js/*.js', 'build/css/*.css'])
         .pipe(clean());
 });
 
-gulp.task('watch', function(){
-  reload.listen({reloadPage: 'index.html'});
-  gulp.watch([paths.sourceJs, paths.sourceSass], ['build']);
-});
 
 gulp.task('build', ['clean', 'scripts:concat-min', 'sassy:min'], function(){
   utils.log('################ Gulp Watching for Changes! ################');
 });
 
-gulp.task('default', ['build', 'watch'], function(){
+gulp.task('default', ['build', 'browser:sync'], function(){
   utils.log('################ Gulp Default Process Running! ################');
 });
