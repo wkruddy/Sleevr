89a7fac19d90e70618df12ca4e0c08509bab6717
Kyle Ruddy <wkruddy@gmail.com>
2016-02-06 12:25:31 -0500

    Forgot to add my express file in the last commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Basic express server that serves static files
+// from inside the root directory, and sends the base HTML
+var express = require('express');
+var path = require('path');
+var app = express();
+var router = express.Router();
+
+// __dirname  === default `pwd` path
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: __dirname});
+});
+
+app.listen(9000);
