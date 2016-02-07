a4828f0fe9213bf1b9f7fce603c4e865a14733aa
Kyle Ruddy <wkruddy@gmail.com>
2016-02-07 00:39:37 -0500

    Refactored the Gulpfile to make it almost production ready. Changed the handling of Bootstrap to now be the Sass implementation. Added a Libs dir that will probably get tweaked to reference just the node_module files. Changed the index entry HTML for a bit of Bootstrap fun.  Added some Gulp plugins for tracking filesize. Applied Autoprefixer.

--- gulpfile.js
+++ gulpfile.js
@@ -10,26 +10,49 @@ var gulp = require('gulp'),
     utils = require('gulp-util'),
     nano = require('gulp-cssnano'),
     sass = require('gulp-sass'),
+    size = require('gulp-size'),
     browserSync = require('browser-sync').create(),
     reload = browserSync.reload,
     autoprefixer = require('gulp-autoprefixer'),
     nodemon = require('gulp-nodemon');
 
+/*
+########################
+######## Pathing #######
+########################
+*/
+
 var paths = {
   sourceJs: 'source/js/*.js',
   sourceSass: 'source/sass/*.scss',
-  buildJs: 'build/js',
-  buildCss: 'build/css'
+  buildJs: 'build/js/',
+  buildCss: 'build/css/',
+  libJs: 'lib/js/',
+  libSass: 'lib/sass/'
 };
 
-// Initialize BrowserSync with the port BS should run on,
-// and the express server port
-gulp.task('browser:sync', ['nodemon'], function() {
-  browserSync.init(null, {
-    proxy: 'http://localhost:9000',
-    port: 5000
-  });
+/*
+########################
+#### Start Up Tasks ####
+########################
+*/
+
+gulp.task('build', ['clean', 'lib:compile', 'scripts:compression', 'sassy:compression'], function(){
+  utils.log('################ Gulp Watching for Changes! ################');
 });
+
+gulp.task('default', ['build', 'browser:sync'], function(){
+  utils.log('################ Gulp Default Process Running! ################');
+  gulp.watch('*.html', reload);
+  gulp.watch(paths.sourceJs, ['lib:combine:js', 'scripts:clean', 'scripts:compression']);
+  gulp.watch(paths.sourceSass, ['lib:combine:sass','sassy:clean', 'sassy:compression']);
+});
+
+/*
+########################
+###### Boot Tasks ######
+########################
+*/
 
 // Task for starting nodemon to use a local express server
 gulp.task('nodemon', function (callback) {
@@ -40,7 +63,8 @@ gulp.task('nodemon', function (callback) {
     script: 'app.js',
     ignore: [
       'gulpfile.js',
-      'node_modules/'
+      'node_modules/',
+      paths.sourceJs
     ]
   }).on('start', function () {
     // Standard pattern for making sure nodemon doesn't init a bunch
@@ -52,57 +76,128 @@ gulp.task('nodemon', function (callback) {
   });
 });
 
+// Initialize BrowserSync with the port BS should run on,
+// and the express server port
+gulp.task('browser:sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    proxy: 'http://localhost:9000',
+    port: 5000
+  });
+});
+
+/*
+########################
+### JavaScript Tasks ###
+########################
+*/
+
 gulp.task('lint', function(){
   return gulp.src(paths.sourceJs)
         .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(reload({stream: true}));
+        .pipe(jshint.reporter('default'));
 });
 
-gulp.task('scripts:concat-min', ['lint'], function(){
-  return gulp.src([paths.sourceJs,
-                  'node_modules/jquery/dist/jquery.js',
-                  'node_modules/bootstrap/dist/js/bootstrap.js'])
+gulp.task('scripts:compression', ['lint'], function(){
+  return gulp.src(paths.sourceJs)
+        .pipe(size({
+          showFiles: true,
+          title: '######## Initial ----Core---- JS size ########'
+        }))
         .pipe(concat('scripts'))
         .pipe(rename({
           extname: '.min.js'
         }))
         .pipe(uglify())
+        .pipe(size({
+          showFiles: true,
+          title: '######## Final compressed ----Core---- JS size ########'
+        }))
         .pipe(gulp.dest('build/js'))
         .pipe(reload({stream: true}));
 });
 
-
-gulp.task('sass:compile', function(){
-  return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css',
-                  'node_modules/bootstrap/dist/css/bootstrap-theme.css',
-                  'source/sass/styles.scss'
-        ])
-        .pipe(sass())
-        .pipe(gulp.dest(paths.buildCss));
+gulp.task('lib:combine:js', function(){
+  // Bootstrap depends on jQuery being in front of it
+  return gulp.src([paths.libJs + 'jquery.js', paths.libJs + '*.js'])
+        .pipe(size({
+          showFiles: true,
+          title: '######## Initial ----Lib---- JS size ########'
+        }))
+        .pipe(concat('lib'))
+        .pipe(rename({
+          extname: '.min.js'
+        }))
+        .pipe(uglify())
+        .pipe(size({
+          showFiles: true,
+          title: '####### Final compressed ----Lib---- JS size ########'
+        }))
+        .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('sassy:min', ['sass:compile'], function(){
-  return gulp.src('build/css/*.css')
+/*
+########################
+###### Sass Tasks ######
+########################
+*/
+
+gulp.task('sassy:compression', function(){
+  return gulp.src('source/sass/styles.scss')
+        .pipe(size({
+          showFiles: true,
+          title: '######## Initial ----Core---- SASS size #######'
+        }))
+        .pipe(sass({errLogToConsole: true}))
         .pipe(concat('styles'))
         .pipe(rename({
           extname: '.min.css'
         }))
         .pipe(nano())
+        .pipe(size({
+          showFiles: true,
+          title: '####### Final ----Core---- SASS size #######'
+        }))
+        .pipe(gulp.dest(paths.buildCss))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('lib:combine:sass', function(){
+  return gulp.src([paths.libSass + 'bootstrap/bootstrap.scss'])
+        .pipe(size({
+          showFiles: true,
+          title: '######## Initial ----Lib---- SASS size #######'
+        }))
+        .pipe(sass({errLogToConsole: true}))
+        .pipe(autoprefixer({
+          browsers: ['last 2 versions']
+        }))
+        .pipe(concat('lib'))
+        .pipe(rename({
+          extname: '.min.css'
+        }))
+        .pipe(nano())
+        .pipe(size({
+          showFiles: true,
+          title: '####### Final compressed ----Lib---- SASS size #######'
+        }))
         .pipe(gulp.dest(paths.buildCss));
 });
 
+/*
+########################
+#### Clean Up Tasks ####
+########################
+*/
+gulp.task('clean', ['scripts:clean', 'sassy:clean']);
 
-gulp.task('clean', function(){
-  return gulp.src(['build/js/*.js', 'build/css/*.css'])
+gulp.task('lib:compile', ['lib:combine:sass', 'lib:combine:js']);
+
+gulp.task('scripts:clean', function(){
+  return gulp.src(paths.buildJs + '*.js')
         .pipe(clean());
 });
 
-
-gulp.task('build', ['clean', 'scripts:concat-min', 'sassy:min'], function(){
-  utils.log('################ Gulp Watching for Changes! ################');
-});
-
-gulp.task('default', ['build', 'browser:sync'], function(){
-  utils.log('################ Gulp Default Process Running! ################');
+gulp.task('sassy:clean', function(){
+  return gulp.src(paths.buildCss + '*.css')
+        .pipe(clean());
 });
--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,3 @@
 (function(){
-	console.log('fuck!!');
+	// Blank file for now!!!
 })();
